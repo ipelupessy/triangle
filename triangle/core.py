eb50695c75c8c2fd015830c534a2d59a044ded88
F.I. Pelupessy <f.i.pelupessy@casema.nl>
2015-05-06 21:10:36 +0200

    explicit to bytes

--- triangle/core.py
+++ triangle/core.py
@@ -249,7 +249,7 @@ class TriangulateIO(object):
         return numpy.array(self.c.normlist[0:N])
 
 def triang(switch, in_, out_, vorout=None):
-    libtriangle.triangulate(switch, in_.c, out_.c, 
+    libtriangle.triangulate(bytes(switch), in_.c, out_.c, 
       None if vorout is None else vorout.c)
 
     # Copy whole array to avoid freeing of non-allocated pointers
